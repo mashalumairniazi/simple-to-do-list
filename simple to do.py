81b37ba2a9101c64098c3878e1f0602a09049352
mashalumairniazi <kindle.mashal@gmail.com>
2024-08-04 22:21:52 +0500

    Update simple to do.py
    new update (been busy),, currently generates an error, needs to be fixed

--- simple to do.py
+++ simple to do.py
@@ -1,3 +1,12 @@
+def choices():
+    print('1. add an item')
+    print('2. remove an item')
+    print('3. show part of a list')
+    print('4. show entire list')
+    print('5. exit')
+    option = int(input('>>>'))
+
+#intro
 print('Welcome - this is your personal to-do list')
 print(' ')
 name = input('What is your name?')
@@ -6,9 +15,33 @@ print(' ')
 print('What do you want to do?')
 print('CHOOSE:')
 
-print('1. add an item')
-print('2. remove an item')
-print('3. show part of a list')
-print('4. show entire list')
-print('5. exit')
-option = int(input('>>>'))
+
+
+list = []
+
+print('')
+
+#add an item
+if option == 1:
+    print('what do you want to add?')
+    add = str(input('>'))
+    list.append(add)
+    print('list updated')
+    print('')
+    #redoing the loop
+    choices()
+
+
+#remove an item
+elif option ==2:
+    print('what do you want to remove?')
+    print('enter the item no. (0,1,2,3...) from the list')
+    print(list)
+    remove = int(input('>'))
+    del list[remove]
+    print('')
+    print(list)
+    print('')
+    choices()
+
+#incomplete - generates an error
